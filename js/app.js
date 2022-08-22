02027155345dde072cce5b7f61b2768f83944b02
Joy <mmehedihasanjoyv@gmail.com>
2022-08-22 14:37:14 +0600

    completed all calculations function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 const allPlayers = [];
-//  common function
+//  common function for input fields
 function getElementByInputField(IdName) {
     const perPlayersPrice = document.getElementById(IdName);
     const perPlayersPriceString = perPlayersPrice.value;
@@ -8,8 +8,16 @@ function getElementByInputField(IdName) {
     return perPlayersPriceInNum;
   
 }
+// common function 2 for innerText
+function getElementByIdName(element) {
+    const setValue= document.getElementById(element);
+    const valueInString = setValue.innerText;
+    const convertStringTonNum = parseFloat(valueInString);
+    return convertStringTonNum;
 
+}
 
+// first calculate button
 document.getElementById('cal-btn').addEventListener('click', function () {
 
     let check = allPlayers.length;
@@ -17,40 +25,39 @@ document.getElementById('cal-btn').addEventListener('click', function () {
 
     let list = document.querySelector("#Selected-PlayersList");
     let listLi = list.getElementsByTagName("li");
-const listLength = listLi.length;
+    const listLength = listLi.length;
 
-    const perPlayer = document.getElementById('player-expenses');
+    const totalPlayerCost = document.getElementById('player-expenses');
     let total = perPlayersPriceInNum *listLength;
-    perPlayer.innerText = total;
+    totalPlayerCost.innerText = total;
+    return totalPlayerCost;
 
 })
+// second calculate button
+
 document.getElementById('cal2-btn').addEventListener('click', function () {
-    const totalConsumption = document.getElementById('total');
+    const totalPlayerCosts = document.getElementById('total');
+    const totalPlayerCost=getElementByIdName('player-expenses');
+    
     const managerTaka =getElementByInputField('Manager-tk');
-    // console.log(managerTaka);
     const CoachTaka =getElementByInputField('Coach-tk');
-    // console.log(CoachTaka);
-    const CoachAndManegerTaka = managerTaka + CoachTaka;
-    totalConsumption.innerText = CoachAndManegerTaka;
-  
+    
+    let total = managerTaka + CoachTaka;
+
+    const totalCostOfTeam = totalPlayerCost + total;
+    totalPlayerCosts.innerText =totalCostOfTeam;
 
 })
-
-
 
 function getPlayerInfo(element) {
     const playerName = element.parentNode.parentNode.children[0].innerText;
     const playerInfo = { playerName: playerName }
-  
- 
+
     allPlayers.push(playerInfo);
-
     let selectedPlayers = allPlayers.length;
-
     const perPlayersPrice = document.getElementById('per-PlayerPrice');
-   
-    if (selectedPlayers <6 ) { 
-        
+
+    if (selectedPlayers <6 ) {    
         let ol = document.getElementById('Selected-PlayersList');
         let li = document.createElement('li');
         li.innerText = playerName;
