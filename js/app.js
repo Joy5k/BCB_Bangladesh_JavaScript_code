ee8d87ee39fda1459c46b553bec442e94c790ebc
Joy <mmehedihasanjoyv@gmail.com>
2022-08-22 12:46:34 +0600

    added per Player Price

--- js/app.js
+++ js/app.js
@@ -4,19 +4,26 @@ const allPlayers = [];
 document.getElementById('cal-btn').addEventListener('click', function () {
     // const inputTaka = getElementByInputField('per-PlayerPrice');
     let check = allPlayers.length;
-    console.log(check);
+    // console.log(check);
     
+
+
+ 
     // const perPlayersPriceTaka = parseInt(perPlayerString);
+    let list = document.querySelector("#Selected-PlayersList");
+    let listLi = list.getElementsByTagName("li");
+const listLength = listLi.length;
+
 
     const perPlayer = document.getElementById('player-expenses');
     const perPlayersPrice = document.getElementById('per-PlayerPrice');
     const perPlayersPriceString = perPlayersPrice.value;
     const perPlayersPriceInNum = parseFloat(perPlayersPriceString);
-    let total = perPlayersPriceInNum *10;
+    console.log(perPlayersPriceInNum);
+    let total = perPlayersPriceInNum *listLength;
     perPlayer.innerText = total;
-//     const perPlayerString = perPlayer.innerText;
-// perPlayer.innerText = total;
-    console.log('its total=',total);
+    console.log('its total=', total);
+   
 
 
 })
@@ -26,39 +33,28 @@ function getElementByInputField(IdName) {
     const inputValue = document.getElementById(IdName);
     const inputValueInString = inputValue.value;
     const inputNumber = parseFloat(inputValueInString);
-    
-    
+
     return inputNumber;
   
 }
 
 function getPlayerInfo(element) {
     const playerName = element.parentNode.parentNode.children[0].innerText;
-
-    // const playerNameString = playerName.innerText;
-    // const playerNameconvert = parseInt(playerNameString);
-
-    // const perPlayer = document.getElementById('player-expenses');
     const playerInfo = { playerName: playerName }
   
  
     allPlayers.push(playerInfo);
 
     let selectedPlayers = allPlayers.length;
-   
-   
-       
-    const perPlayersPrice = document.getElementById('per-PlayerPrice');
 
+    const perPlayersPrice = document.getElementById('per-PlayerPrice');
+   
     if (selectedPlayers <6 ) { 
         
         let ol = document.getElementById('Selected-PlayersList');
         let li = document.createElement('li');
         li.innerText = playerName;
         ol.appendChild(li);
-        let numb = document.getElementById("per-PlayerPrice").childElementCount;
-        
-        console.log(numb + 1);
     }
    
     else {
@@ -66,15 +62,6 @@ function getPlayerInfo(element) {
     }
   
 }
-
-
-
-
-
-
-
-
- // const selectedPlayers = document.getElementById('Selected-PlayersList').childElementCount;
 
    
    
